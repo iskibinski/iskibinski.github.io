c6b4ed1a8a4a4cacfac2319fa9fc470a07b43dfc
iskibinski <skibinskiisaac@gmail.com>
2020-10-20 14:39:09 -0700

    remove random colors

--- body_background.js
+++
@@ -1,5 +0,0 @@
-// the page this is loaded on must load randomColor.js beforehand
-var random_color1 = randomColor({luminosity: 'dark', hue: 'blue'});
-var random_color2 = randomColor({luminosity: 'light', hue: 'yellow'});
-var gradient_string = 'linear-gradient(to bottom right, ' + random_color1 + ', ' + random_color2 + ')';
-document.body.style.background = gradient_string;
